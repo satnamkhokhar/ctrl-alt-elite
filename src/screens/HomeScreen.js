29a779be4149218dc905701aa63b2cbbd58caaad
Dovahkiin314 <dovahkiin3.14159265358979323@gmail.com>
2026-04-01 05:30:14 -0400

    Update HomeScreen.js
    Should be updated now

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
 
 function HomeScreen() {
+    const navigation = useNavigation();
+
     return (
         <SafeAreaProvider>
-            <SafeAreaView>
-                <Text>Home Screen - Coming Soon</Text>
+            <SafeAreaView style={styles.container}>
+                <Text style={styles.title}>Home Screen</Text>
+
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => navigation.navigate('SessionScreen')}
+                >
+                    <Text style={styles.buttonText}>Start Session</Text>
+                </TouchableOpacity>
             </SafeAreaView>
         </SafeAreaProvider>
     );
 }
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'pink',
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 20,
+    },
+    button: {
+        backgroundColor: 'mediumspringgreen',
+        borderWidth: 2,
+        borderColor: 'black',
+        padding: 10,
+        borderRadius: 8,
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
