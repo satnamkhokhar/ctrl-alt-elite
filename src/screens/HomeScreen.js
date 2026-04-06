a8009347ff22510a33452cc7df235b6fcf00b53e
aboakye1994 <98774793+aboakye1994@users.noreply.github.com>
2026-04-06 07:55:26 -0400

    Update HomeScreen.js
    Resolve HomeScreen merge conflict - keep gradient design, navigate to SessionScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,49 +1,188 @@
-import React from 'react';
-import { Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 function HomeScreen() {
     const navigation = useNavigation();
 
     return (
-        <SafeAreaProvider>
+        <SafeAreaProvider>    
+           <LinearGradient
+            colors={['#f00b0bff', '#c76d18ff']}
+            style={styles.container}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+            > 
             <SafeAreaView style={styles.container}>
-                <Text style={styles.title}>Home Screen</Text>
-
-                <TouchableOpacity
-                    style={styles.button}
+                <Text style={styles.DineSync}>{'\n'}DineSync</Text>
+                <Text style={styles.rowTwo}>{'\n\n\n\n'}Price Range            Cuisine              Location</Text> 
+                
+                <TouchableOpacity 
+                    style={styles.button} 
                     onPress={() => navigation.navigate('SessionScreen')}
-                >
+                > 
                     <Text style={styles.buttonText}>Start Session</Text>
+                </TouchableOpacity> 
+            
+                <TouchableOpacity style={styles.logoButton}>
+                    <Image
+                        source={require('../../assets/dollar-sign.png')}
+                        style={styles.dollarSign}
+                    />
                 </TouchableOpacity>
+                <TouchableOpacity style={styles.logoButton}>
+                    <Image
+                        source={require('../../assets/cuisine.png')}
+                        style={styles.cuisine}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.logoButton}>
+                    <Image
+                        source={require('../../assets/map.png')}
+                        style={styles.map}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.logoButton}>
+                    <Image
+                        source={require('../../assets/share.png')}
+                        style={styles.share}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.logoButton}>
+                    <Image
+                        source={require('../../assets/profile.png')}
+                        style={styles.profile}
+                    />
+                </TouchableOpacity>
+                
+                <TouchableOpacity style={styles.logoButton}>
+                    <Image
+                        source={require('../../assets/star.png')}
+                        style={styles.star}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.logoButton}>
+                    <Image
+                        source={require('../../assets/search.png')}
+                        style={styles.search}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.logoButton}>
+                    <Image
+                        source={require('../../assets/group.png')}
+                        style={styles.group}
+                    />
+                </TouchableOpacity>
+
             </SafeAreaView>
+            </LinearGradient>
         </SafeAreaProvider>
     );
 }
 
-export default HomeScreen;
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'pink',
+        justifyContent: 'center',
     },
-    title: {
-        fontSize: 20,
-        marginBottom: 20,
+    DineSync: {
+        fontSize: 75,
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        color: 'white',
     },
+    rowTwo: {
+        fontSize: 12,
+        fontWeight: 'bold',
+        color: 'white',
+    }, 
     button: {
-        backgroundColor: 'mediumspringgreen',
+        alignItems: 'center',
+        borderColor:'white',
         borderWidth: 2,
-        borderColor: 'black',
-        padding: 10,
+        height: 50,
+        width: 300,
         borderRadius: 8,
+        marginTop: 15,
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        opacity: .45,
+    },
+    logoButton: {
+        alignItems: 'center',
+        height: 30,
+        width: 150,
+        marginBottom: 8,
+        justifyContent: 'center',
     },
     buttonText: {
-        fontSize: 16,
-        fontWeight: 'bold',
+        color:'#f00b0bff',
+        fontSize: 25,
+        fontWeight:'bold',
+        textAlign:'center',
+    },
+    dollarSign: {
+        position: 'absolute',
+        top:  -140,
+        left: -40,
+        width: 50,
+        height: 50,
+    },
+    cuisine: {
+        position: 'absolute',
+        top: -180,
+        left: 55,
+        width: 60,
+        height: 60,
+    },
+    map: {
+        position: 'absolute',
+        top: -210,
+        left: 150,
+        width: 45,
+        height: 45,
+    },
+    share: {
+        position: 'absolute',
+        top: -440,
+        left: -100,
+        width: 55,
+        height: 55,
+    },
+    profile: {
+        position: 'absolute',
+        top: -475,
+        left: 200,
+        width: 50,
+        height: 50,
+    },
+    star: {
+        position: 'absolute',
+        top: 60,
+        left: -75,
+        width: 50,
+        height: 50,
+    },
+    search: {
+        position: 'absolute',
+        top: 25,
+        left: 60,
+        width: 45,
+        height: 45,
+    },
+    group: {
+        position: 'absolute',
+        top: -40,
+        left: 155,
+        width: 100,
+        height: 100,
     },
 });
+
+export default HomeScreen;
